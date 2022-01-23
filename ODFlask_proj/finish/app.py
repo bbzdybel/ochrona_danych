10f2db7f4f3b4f5c239b5e7c68f805c13dc81657
IBartosz Zdybel <zdybelbartosz@gmail.com>
2022-01-23 23:04:20 +0100

    Added new project

---
+++ ODFlask_proj/finish/app.py
@@ -0,0 +1,280 @@
+import hashlib
+import json
+import os
+from time import time
+import jwt
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad, pad
+from flask_mail import Mail, Message
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm, form
+from wtforms import StringField, PasswordField, BooleanField, EmailField
+from wtforms.validators import InputRequired, Email, Length, DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from base64 import b64encode, b64decode
+
+# import sqlite3
+# con = sqlite3.connect('C:\\Users\\zdybe\\Desktop\\ODFlask_proj\\finish\\database.db')
+# con.cursor()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+pepper = b'dfwiubwiubdvbwdbwdvbwuvwdvb'
+encryption_method = 'pbkdf2:sha256:100000'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\zdybe\\Desktop\\ODFlask_proj\\finish\\database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = "9089f97df84c67"
+app.config['MAIL_PASSWORD'] = "1a81aa0531dd79"
+mail = Mail(app)
+
+
+
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    salt = db.Column(db.String(32))
+    passManager = db.relationship("PassManager", backref='user', lazy=True)
+    passwords_key = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f" Id: {self.id} \n Username: {self.username} \n Email: {self.email} \n Password: {self.password} \n"
+
+    def get_reset_token(self, expires=500):
+        return jwt.encode({'reset_password': self.username, 'exp': time() + expires},
+                          key=app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_token(token):
+        try:
+            username = jwt.decode(token, key=app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except Exception as e:
+            print(e)
+            return
+        return User.query.filter_by(username=username).first()
+
+
+class PassManager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    appName = db.Column(db.String(15), unique=True)
+    login = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, appName,login,password, userId):
+        self.appName = appName
+        self.login = login
+        self.password = password
+        self.userId = userId
+
+class ForgotForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email(message='Invalid email'), Length(max=50)])
+
+class PasswordResetForm(FlaskForm):
+    new_password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=80)])
+    confirm = PasswordField('Repeat Password')
+
+
+@app.route('/insert', methods=['POST'])
+@login_required
+def insert():
+
+    if request.method == 'POST':
+        webappFromDash = request.form['webapp']
+        loginFromDash = request.form['login']
+        userID = current_user.id
+        passwordFromDash = encrypt_value(current_user.passwords_key, request.form['password'])
+
+        my_data = PassManager(webappFromDash,loginFromDash,passwordFromDash, userID)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Added new password")
+        return redirect(url_for('dashboard'))
+
+@app.route('/update', methods = ['GET', 'POST'])
+@login_required
+def update():
+    if request.method == 'POST':
+        my_data = PassManager.query.get(request.form.get('id'))
+
+        my_data.appName = request.form['webapp']
+        my_data.login = request.form['login']
+        my_data.password = encrypt_value(current_user.passwords_key, request.form['password'])
+
+        db.session.commit()
+        flash("Passoword Updated Successfully")
+
+        return redirect(url_for('dashboard'))
+
+
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+    my_data = PassManager.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Password Deleted Successfully")
+
+    return redirect(url_for('dashboard'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if hashlib.pbkdf2_hmac('sha256', form.password.data.encode('utf-8'), user.salt + pepper, 100000) == (user.password):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+
+        return '<h1>Invalid username or password</h1>'
+        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        salt = os.urandom(32)
+        hashed_password = hashlib.pbkdf2_hmac('sha256', form.password.data.encode('utf-8'), salt + pepper, 100000)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password, salt=salt, passwords_key=get_key(16))
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+
+def get_key(size):
+    key = os.urandom(size)
+    return key
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    user = User.query.filter_by(username=current_user.username).first()
+    passwod_list = user.passManager
+    visible_passwords_list = []
+
+    for item in passwod_list:
+        visible_passwords_list.append(
+            {'id': item.id, 'appName': item.appName, 'login':  item.login,
+             'password': decrypt_value(current_user.passwords_key, item.password)})
+
+    return render_template('dashboard.html', visible_passwords_list = visible_passwords_list)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/password_reset_verified/<token>', methods=['GET', 'POST'])
+def reset_verified(token):
+
+    form = PasswordResetForm()
+
+    if form.validate_on_submit():
+        user = User.verify_reset_token(token)
+        if not user:
+            return redirect(url_for('index'))
+
+        method, salt, hash = generate_password_hash(form.new_password.data + pepper, method=encryption_method,
+                                                    salt_length=24).split('$')
+        hashed_password = salt + '$' + hash
+        user.password = hashed_password
+        db.session.commit()
+        flash('Password changed successfully!')
+        return redirect(url_for('index'))
+
+    return render_template('reset_verified.html', form=form)
+
+def send_email(user):
+    token = user.get_reset_token()
+
+    msg = Message()
+    msg.subject = "Password Reset"
+    msg.sender = app.config['MAIL_USERNAME']
+    msg.recipients = [user.email]
+    msg.html = render_template('reset_your_password.html', user=user, token=token)
+
+    mail.send(msg)
+
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot():
+    form = ForgotForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user:
+            send_email(user)
+
+        flash('You will receive an email, if we find you in our databse.')
+        return redirect(url_for('index'))
+
+    return render_template('forgot.html', form=form)
+
+
+def encrypt_value(key, data_to_encrypt):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(data_to_encrypt.encode(), AES.block_size))
+    iv = b64encode(cipher.iv).decode('utf-8')
+    ct = b64encode(ct_bytes).decode('utf-8')
+    result = json.dumps({'iv': iv, 'ciphertext': ct })
+    return result
+
+
+def decrypt_value(key, data_to_decrypt):
+    data_to_decrypt = json.loads(data_to_decrypt)
+    iv = b64decode(data_to_decrypt['iv'])
+    ct = b64decode(data_to_decrypt['ciphertext'])
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    pt = unpad(cipher.decrypt(ct), AES.block_size)
+    return pt.decode('utf-8')
+
+if __name__ == '__main__':
+    app.run(debug=True, ssl_context=('C:\\Users\\zdybe\\Desktop\\ODFlask_proj\\finish\\cert.pem', 'C:\\Users\\zdybe\\Desktop\\ODFlask_proj\\finish\\key.pem'))
